90065ac3af50d942cf0fdcdc2c18f7e1ab54a7bb
MAHBUB-HASAN-HIRA <mahbubhasan89.diu@gmail.com>
2020-10-28 23:28:58 +0600

    send order data to database from CustomerOrder Components

--- src/Components/Customer/Order/OrderForm/OrderForm.js
+++ src/Components/Customer/Order/OrderForm/OrderForm.js
@@ -1,28 +1,75 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './OrderForm.css';
 import uploadLogo from'../../../../creative-agency-resources/images/uploadLogo.PNG';
+import { UserContext } from '../../../../App';
+
 const OrderForm = () => {
+    const [loggedInUser] = useContext(UserContext);
+    const [orderDetail, setOrderDetail] = useState({});
+    const [file, setFile] = useState(null);
+
+    const handleChange = (e) => {
+       const newOrderDetail = {...orderDetail};
+       newOrderDetail[e.target.name] = e.target.value;
+       newOrderDetail['status'] = 'Pending';
+       setOrderDetail(newOrderDetail);
+    }
+
+    const handleFileChange = (e) => {
+        const newFile = e.target.files[0];
+        setFile(newFile);
+    }
+
+    const handleOrderDetailSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData()
+        formData.append('file', file);
+        formData.append('name', orderDetail.name);
+        formData.append('orderEmail', orderDetail.orderEmail);
+        formData.append('userEmail', loggedInUser.email);
+        formData.append('service', orderDetail.service);
+        formData.append('message', orderDetail.message);
+        formData.append('price', orderDetail.price);
+        formData.append('status', orderDetail.status);
+      
+            fetch('http://localhost:5000/addOrder', {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => response.json())
+                .then(data => {
+                console.log(data);
+                alert('order Successfully Placed. Thanks For Order');
+                setOrderDetail({});
+                setFile(null);
+                window.location.reload();
+            })
+                .catch(error => {
+                console.error(error)
+            })
+    }
+
     return (
         <div className='form_container'>
-            <form> 
+            <form onSubmit={handleOrderDetailSubmit}> 
                 <div className="form-group">
-                    <input type="text" className="form-control"  placeholder="Your name / Company's name"/>
+                    <input type="text" onChange={handleChange} name='name' className="form-control"  placeholder="Your name / Company's name" required/>
                 </div>
                 <div className="form-group">
-                    <input type="email" className="form-control"  placeholder="Your email address"/>
+                    <input type="email" onChange={handleChange} name='orderEmail' className="form-control"  placeholder="Your email address" required/>
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control"  placeholder="Service Name"/>
+                    <input type="text" onChange={handleChange} name='service' className="form-control"  placeholder="Service Name" required/>
                 </div>
                 <div className="form-group">
-                    <textarea  className="form-control" placeholder='Your massage' rows="4" />
+                    <textarea  onChange={handleChange} name='message' className="form-control" placeholder='Your message' rows="4"  required/>
                 </div>
                 <div className="form-row mb-3 d-flex align-items-center">
                     <div className="form-group col-sm-6 my-1">
-                        <input type="text" className="form-control" placeholder="Price"/>
+                        <input type="text" onChange={handleChange} name='price' className="form-control" placeholder="Price" required/>
                     </div>
                     <div className="form-group col-sm-6 my-1">
-                        <input type="file" name="file-1[]" id="file-1" className="inputFile inputFile-1" data-multiple-caption="{count} files selected" multiple=""/>
+                        <input type="file" onChange={handleFileChange} name="file" id="file-1" className="inputFile inputFile-1" data-multiple-caption="{count} files selected" multiple=""/>
                         <label className='upload_label' for="file-1"><img className='upload_img' src={uploadLogo} alt="upload"/> <span>Upload project img</span></label>
                     </div>
                 </div>
